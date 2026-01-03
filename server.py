f0040d10ec057202ae7ddb72ae2daa17181cc8f9
shehadehmohammad099-create <shehadehmohammad099@gmail.com>
2026-01-03 17:35:52 +0000

    Update server.py

--- server.py
+++ server.py
@@ -73,7 +73,7 @@ def create_checkout_session(request: Request):
     session = stripe.checkout.Session.create(
         mode="subscription",
         line_items=[{"price": STRIPE_PRICE_ID, "quantity": 1}],
-        success_url=f"{origin}/frontend/index.html?session_id={{CHECKOUT_SESSION_ID}}",
+        success_url=f"{origin}/frontend/static/app.html?session_id={{CHECKOUT_SESSION_ID}}",
         cancel_url=f"{origin}/index.html",
     )
 
