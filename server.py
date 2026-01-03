78065db79b0330844f310ad8881387b1371fc532
shehadehmohammad099-create <shehadehmohammad099@gmail.com>
2026-01-03 00:18:21 +0000

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://frabjous-begonia-1cbd54.netlify.app"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
