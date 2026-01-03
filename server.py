be446bb0f8f2e9cfbb656f24ec8faf3d92dc4322
shehadehmohammad099-create <shehadehmohammad099@gmail.com>
2026-01-03 17:30:59 +0000

    Update server.py

--- server.py
+++ server.py
@@ -61,16 +61,22 @@ class ExplainWord(BaseModel):
 # STRIPE
 # -------------------------
     
-FRONTEND_URL = "https://the-lexicon-project.netlify.app"
+
 @app.get("/create-checkout-session")
-def create_checkout_session():
+def create_checkout_session(request: Request):
+    origin = request.headers.get("origin")
+
+    # Fallback to prod if origin missing
+    if not origin:
+        origin = "https://the-lexicon-project.netlify.app"
+
     session = stripe.checkout.Session.create(
         mode="subscription",
         line_items=[{"price": STRIPE_PRICE_ID, "quantity": 1}],
-        success_url=f"{FRONTEND_URL}/static/confirmation.html?session_id={{CHECKOUT_SESSION_ID}}",
-        # success_url=f"{FRONTEND_URL}/static/confirmation.html?session_id={{CHECKOUT_SESSION_ID}}",
-        cancel_url=FRONTEND_URL,
+        success_url=f"{origin}/index.html?session_id={{CHECKOUT_SESSION_ID}}",
+        cancel_url=f"{origin}/index.html",
     )
+
     return {"url": session.url}
 
 @app.get("/checkout-success")
