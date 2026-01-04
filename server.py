d681a109cc00379b654a8f73ec35e94db4f7f01b
shehadehmohammad099-create <shehadehmohammad099@gmail.com>
2026-01-04 11:31:37 +0000

    Update server.py

--- server.py
+++ server.py
@@ -185,7 +185,7 @@ def send_restore_email(to_email: str, restore_url: str):
             "Content-Type": "application/json",
         },
         json={
-            "from": "Lexikon <restore@lexikon.app>",
+            "from": "shehadehmohammad099@gmail.com",
             "to": to_email,
             "subject": "Restore your Lexikon subscription",
             "html": f"""
